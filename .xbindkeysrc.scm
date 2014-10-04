48528d82418bc506975af45ac40fc83b7dcf2ca7
Mikael Svantesson <mikael@distopic.net>
2014-10-04 16:15:34 +0200

    Added .xbindkeysrc and warppointer script.

---
+++ .xbindkeysrc.scm
@@ -0,0 +1,19 @@
+(xbindkey '(control shift t) "urxvt")
+; (xbindkey '(control shift f) "firefox")
+; (xbindkey '(control shift f) "opera")
+(xbindkey '(control shift f) "x-www-browser")
+(xbindkey '(control shift g) "gvim")
+
+(xbindkey '(control shift a) "audio_volume increase")
+(xbindkey '(control shift o) "audio_volume decrease")
+
+(xbindkey '(control shift m) "mpc prev")
+(xbindkey '(control shift w) "mpc toggle")
+(xbindkey '(control shift v) "mpc stop")
+(xbindkey '(control shift z) "mpc next")
+
+(xbindkey '(mod5 "c:10") "warppointer 1")
+(xbindkey '(mod5 "c:11") "warppointer 0")
+(xbindkey '(mod5 "c:12") "warppointer 2")
+(xbindkey '(mod5 "c:13") "warppointer 3")
+
