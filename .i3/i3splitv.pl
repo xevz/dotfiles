9d782de619322e18f3d8fbc94edcf11eaa32ae78
Mikael Svantesson <mikael@distopic.net>
2015-08-24 19:28:39 +0200

    i3: Make i3workspace behave on i3 exit/restart.

--- .i3/i3splitv.pl
+++ .i3/i3splitv.pl
@@ -1,13 +1,5 @@
 #!/usr/bin/env perl
 
-=head1 SYNOPSIS
-
-Start using
-
-start-stop-daemon --start --make-pidfile --background --pidfile $HOME/.i3/splitv.pid --exec $HOME/.i3/i3splitv.pl
-
-=cut
-
 use strict;
 use warnings;
 
@@ -21,7 +13,7 @@ $i3->subscribe({
         window => sub {
             my $e = shift;
 
-            return unless $e->{container}->{layout} ;
+            return unless $e->{container}->{layout};
 
             if ($e->{container}->{layout} eq 'splith') {
                 $i3->message(TYPE_COMMAND, 'split v');
--- .i3/i3workspace.pl
+++ .i3/i3workspace.pl
@@ -1,39 +1,46 @@
 #!/usr/bin/env perl
 
-=head1 SYNOPSIS
-
-Start using
-
-start-stop-daemon --start --make-pidfile --background --pidfile $HOME/.i3/workspace.pid --exec $HOME/.i3/i3workspace.pl
-
-=cut
-
 use strict;
 use warnings;
 
+use POSIX;
+
+use AnyEvent;
 use AnyEvent::I3 qw(:all);
+
+my $WORKSPACES = 10;
 
 my $i3 = i3();
 
-$i3->connect->recv;
+$i3->connect->recv or die "Could not connect to i3: $!";
 
-$i3->subscribe({
-        workspace => sub {
-            my $e = shift;
+my %callbacks = (
+    workspace => sub {
+        my $e = shift;
 
-            my $name = $e->{'current'}->{name};
+        my $num = $e->{'current'}->{num};
+        my $monitor = ceil($num / $WORKSPACES);
 
-            if ($name <= 10) {
-                $i3->message(TYPE_COMMAND, 'mode default');
-            }
-            elsif ($name > 10 && $name <= 20) {
-                $i3->message(TYPE_COMMAND, 'mode monitor2');
-            }
-            elsif ($name > 20 && $name <= 30) {
-                $i3->message(TYPE_COMMAND, 'mode monitor3');
-            }
+        if ($monitor == 1) {
+            $i3->message(TYPE_COMMAND, 'mode default');
+        } else {
+            $i3->message(TYPE_COMMAND, "mode monitor${monitor}");
         }
-    })->recv;
+    },
+    _error => sub {
+        my $msg = shift;
+
+        unless ($msg eq 'Unexpected end-of-file') {
+            print STDERR "An error occured: $msg";
+            exit 1;
+        }
+
+        exit 0;
+    }
+);
+
+$i3->subscribe(\%callbacks)->recv->{success}
+    or die "Could not subscribe to the events: $!";
 
 AE::cv->recv;
 
