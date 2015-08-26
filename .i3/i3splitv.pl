943796368b406eb549ea7ab93fc68ece5072a618
Mikael Svantesson <mikael@distopic.net>
2015-08-26 19:46:04 +0200

    i3: Finally made i3splitv work as expected.

--- .i3/i3splitv.pl
+++ .i3/i3splitv.pl
@@ -3,23 +3,39 @@
 use strict;
 use warnings;
 
+use AnyEvent;
 use AnyEvent::I3 qw(:all);
 
 my $i3 = i3();
 
-$i3->connect->recv;
+$i3->connect->recv or die "Could not connect to i3: $!";
 
-$i3->subscribe({
-        window => sub {
-            my $e = shift;
+my %callbacks = (
+    window => sub {
+        my $e = shift;
 
-            return unless $e->{container}->{layout};
+        return unless $e->{change} eq 'move';
+        return unless $e->{container}->{layout};
 
-            if ($e->{container}->{layout} eq 'splith') {
-                $i3->message(TYPE_COMMAND, 'split v');
-            }
+        if ($e->{container}->{layout} eq 'splith') {
+            $i3->message(TYPE_COMMAND, 'split v');
         }
-    })->recv;
+
+    },
+    _error => sub {
+        my $msg = shift;
+
+        unless ($msg eq 'Unexpected end-of-file') {
+            print STDERR "An error occured: $msg";
+            exit 1;
+        }
+
+        exit 0;
+    }
+);
+
+$i3->subscribe(\%callbacks)->recv->{success}
+    or die "Could not subscribe to the events: $!";
 
 AE::cv->recv;
 
