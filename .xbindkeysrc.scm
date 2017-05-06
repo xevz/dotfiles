bf98ff98b9042af6349e4414860274c5d90fe44d
Mikael Svantesson <mikael@distopic.net>
2017-05-06 22:36:00 +0200

    New keybindings.

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -7,6 +7,10 @@
 (xbindkey '(control shift a) "audio_volume increase")
 (xbindkey '(control shift o) "audio_volume decrease")
 
+(xbindkey '(XF86AudioRaiseVolume) "audio_volume increase")
+(xbindkey '(XF86AudioLowerVolume) "audio_volume decrease")
+(xbindkey '(XF86AudioMute) "audio_volume mute")
+
 (xbindkey '(control shift m) "mpc prev")
 (xbindkey '(control shift w) "mpc toggle")
 (xbindkey '(control shift v) "mpc stop")
@@ -16,4 +20,8 @@
 (xbindkey '(mod5 "c:11") "warppointer 0")
 (xbindkey '(mod5 "c:12") "warppointer 2")
 (xbindkey '(mod5 "c:13") "warppointer 3")
+
+(xbindkey '(shift Print) "scrot -e 'mv $f ~/.screenshots'")
+
+(xbindkey '(shift mod1 p) "passmenu --type")
 
